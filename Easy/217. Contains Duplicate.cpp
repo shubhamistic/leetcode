3eb195cc5d59e2d13e0430b27b1cd252a367fe77
Shubham <shubham.219301072@muj.manipal.edu>
2022-12-30 21:14:43 +0530

    added Easy/217. Contains Duplicate.cpp

---
+++ Easy/217. Contains Duplicate.cpp
@@ -0,0 +1,14 @@
+#include <unordered_map>
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> map;
+        for(int i=0 ; i < nums.size() ; i++){
+            if(++map[nums[i]] > 1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
