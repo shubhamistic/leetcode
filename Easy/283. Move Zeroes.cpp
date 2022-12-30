c8493f2c72e7d788a8649b8c73cb371494462d0d
Shubham <shubham.219301072@muj.manipal.edu>
2022-12-30 23:02:06 +0530

    added Easy/283. Move Zeroes.cpp

---
+++ Easy/283. Move Zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int zeroCount = 0;
+        for(int i=0 ; i < nums.size() ; i++){
+            if(nums[i] == 0){
+                zeroCount++;
+            }
+            else if (zeroCount > 0){
+                nums[i-zeroCount] = nums[i];
+                nums[i] = 0;
+            }
+        }
+    }
+};
