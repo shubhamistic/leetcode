1f5d993ef2b41f4834bca908140c20b668b28172
Shubham <shubham.219301072@muj.manipal.edu>
2023-01-13 20:05:48 +0530

    added Easy/575. Distribute Candies.cpp

---
+++ Easy/575. Distribute Candies.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        unordered_set<int> set;
+        int count = 0;
+        for(int i=0 ; i < candyType.size() ; i++){
+            if(set.count(candyType[i]) == 0){
+                set.insert(candyType[i]);
+                count++;
+            }
+        }
+        
+        if(candyType.size()/2 <= count){
+            return candyType.size()/2;
+        }
+        else{
+            return count;
+        }
+    }
+};
